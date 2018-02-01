62250f687416fa644da538c63e9f7b9d2d5c577c
MrYsLab <MrYsLab@gmail.com>
2018-02-01 10:44:24 -0500

    Version 1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='s2m',
-    version='1.4',
+    version='2.0',
     packages=['s2m'],
     install_requires=['pyserial>=2.7',
                       'psutil'],
