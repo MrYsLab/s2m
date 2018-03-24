5cf66602f76d2800f722894eba96dc49aeaf4960
MrYsLab <MrYsLab@gmail.com>
2018-03-24 15:59:50 -0400

    Version 2.4

--- s2m/micro_bit_scripts/s2mb.py
+++ s2m/micro_bit_scripts/s2mb.py
@@ -14,10 +14,11 @@
  along with this library; if not, write to the Free Software
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
- Last modified 07 December 2017
+ Last modified 24 March 2018
 """
 
 from microbit import *
+
 
 # This is a script used to control a micro:bit from s2m
 
@@ -35,195 +36,211 @@ from microbit import *
 #   t - digital write for the specified pin and value (0 or 1)
 #   v - get version string
 
-while True:
-    data = uart.readline()
-    sleep(8)
-    if data:
-        cmd = str(data, 'utf-8').rstrip()
-        if not len(cmd):
-            continue
-        # noinspection PyUnresolvedReferences
-        cmd_list = cmd.split(",")
-        # get command id
 
-        try:
-            cmd_id = cmd_list[0]
-        except IndexError:
-            cmd_id = 'z'
-            continue
-
-        # display image command
-        if cmd_id == 'd':
-            image_dict = {"HAPPY": Image.HAPPY,
-                          "SAD": Image.SAD,
-                          "ANGRY": Image.ANGRY,
-                          "SMILE": Image.SMILE,
-                          "CONFUSED": Image.CONFUSED,
-                          "ASLEEP": Image.ASLEEP,
-                          "SURPRISED": Image.SURPRISED,
-                          "SILLY": Image.SILLY,
-                          "FABULOUS": Image.FABULOUS,
-                          "MEH": Image.MEH,
-                          "YES": Image.YES,
-                          "NO": Image.NO,
-                          "RABBIT": Image.RABBIT,
-                          "COW": Image.COW,
-                          "ROLLERSKATE": Image.ROLLERSKATE,
-                          "HOUSE": Image.HOUSE,
-                          "SNAKE": Image.SNAKE,
-                          "HEART": Image.HEART,
-                          "DIAMOND": Image.DIAMOND,
-                          "DIAMOND_SMALL": Image.DIAMOND_SMALL,
-                          "SQUARE": Image.SQUARE,
-                          "SQUARE_SMALL": Image.SQUARE_SMALL,
-                          "TRIANGLE": Image.TRIANGLE,
-                          "TARGET": Image.TARGET,
-                          "STICKFIGURE": Image.STICKFIGURE,
-                          "ARROW_N": Image.ARROW_N,
-                          "ARROW_NE": Image.ARROW_NE,
-                          "ARROW_E": Image.ARROW_E,
-                          "ARROW_SE": Image.ARROW_SE,
-                          "ARROW_S": Image.ARROW_S,
-                          "ARROW_SW": Image.ARROW_SW,
-                          "ARROW_W": Image.ARROW_W,
-                          "ARROW_NW": Image.ARROW_NW}
-
-            # get image key
-            try:
-                image_key = cmd_list[1]
-            except IndexError:
+# a list of current digital pin modes
+def loop(digital_outputs):
+    while True:
+        data = uart.readline()
+        sleep(8)
+        if data:
+            cmd = str(data, 'utf-8').rstrip()
+            if not len(cmd):
                 continue
-            if image_key in image_dict:
-                display.show(image_dict.get(image_key), wait=False)
-
-        # scroll text command
-        elif cmd_id == 's':
-            display.scroll(str(cmd_list[1]), wait=False)
-
-        # write pixel command
-        elif cmd_id == 'p':
-            # get row, column and intensity value
-            # make sure values are within valid range
-            # print(cmd)
-            try:
-                x = int(cmd_list[1])
-            except ValueError:
-                continue
-            except IndexError:
-                continue
-
-            if x < 0:
-                x = 0
-            if x > 4:
-                x = 4
+            # noinspection PyUnresolvedReferences
+            cmd_list = cmd.split(",")
+            # get command id
 
             try:
-                y = int(cmd_list[2])
-            except ValueError:
-                continue
+                cmd_id = cmd_list[0]
             except IndexError:
+                cmd_id = 'z'
                 continue
 
-            if y < 0:
-                y = 0
-            if y > 4:
-                y = 4
-            try:
-                value = int(cmd_list[3])
-            except ValueError:
-                continue
-            except IndexError:
-                continue
+            # display image command
+            if cmd_id == 'd':
+                image_dict = {"HAPPY": Image.HAPPY,
+                              "SAD": Image.SAD,
+                              "ANGRY": Image.ANGRY,
+                              "SMILE": Image.SMILE,
+                              "CONFUSED": Image.CONFUSED,
+                              "ASLEEP": Image.ASLEEP,
+                              "SURPRISED": Image.SURPRISED,
+                              "SILLY": Image.SILLY,
+                              "FABULOUS": Image.FABULOUS,
+                              "MEH": Image.MEH,
+                              "YES": Image.YES,
+                              "NO": Image.NO,
+                              "RABBIT": Image.RABBIT,
+                              "COW": Image.COW,
+                              "ROLLERSKATE": Image.ROLLERSKATE,
+                              "HOUSE": Image.HOUSE,
+                              "SNAKE": Image.SNAKE,
+                              "HEART": Image.HEART,
+                              "DIAMOND": Image.DIAMOND,
+                              "DIAMOND_SMALL": Image.DIAMOND_SMALL,
+                              "SQUARE": Image.SQUARE,
+                              "SQUARE_SMALL": Image.SQUARE_SMALL,
+                              "TRIANGLE": Image.TRIANGLE,
+                              "TARGET": Image.TARGET,
+                              "STICKFIGURE": Image.STICKFIGURE,
+                              "ARROW_N": Image.ARROW_N,
+                              "ARROW_NE": Image.ARROW_NE,
+                              "ARROW_E": Image.ARROW_E,
+                              "ARROW_SE": Image.ARROW_SE,
+                              "ARROW_S": Image.ARROW_S,
+                              "ARROW_SW": Image.ARROW_SW,
+                              "ARROW_W": Image.ARROW_W,
+                              "ARROW_NW": Image.ARROW_NW}
 
-            if value < 0:
-                value = 0
-            if value > 9:
-                value = 9
-            display.set_pixel(x, y, value)
+                # get image key
+                try:
+                    image_key = cmd_list[1]
+                except IndexError:
+                    continue
+                if image_key in image_dict:
+                    display.show(image_dict.get(image_key), wait=False)
 
-        # clear display command
-        elif cmd_id == 'c':
-            display.clear()
+            # scroll text command
+            elif cmd_id == 's':
+                display.scroll(str(cmd_list[1]), wait=False)
 
-        # analog write command
-        # if values are out of range, command is ignored
-        elif cmd_id == 'a':
-            # check pin and value ranges
-            try:
-                pin = int(cmd_list[1])
-                value = int(cmd_list[2])
-            except IndexError:
-                continue
-            except ValueError:
-                continue
+            # write pixel command
+            elif cmd_id == 'p':
+                # get row, column and intensity value
+                # make sure values are within valid range
+                # print(cmd)
+                try:
+                    x = int(cmd_list[1])
+                except ValueError:
+                    continue
+                except IndexError:
+                    continue
 
-            if 0 <= pin <= 2:
-                if not 0 <= value <= 1023:
-                    value = 256
-                if pin == 0:
-                    pin0.write_analog(value)
-                elif pin == 1:
-                    pin1.write_analog(value)
-                elif pin == 2:
-                    pin2.write_analog(value)
+                if x < 0:
+                    x = 0
+                if x > 4:
+                    x = 4
 
-        # digital write command
-        elif cmd_id == 't':
-            # check pin and value ranges
+                try:
+                    y = int(cmd_list[2])
+                except ValueError:
+                    continue
+                except IndexError:
+                    continue
+
+                if y < 0:
+                    y = 0
+                if y > 4:
+                    y = 4
+                try:
+                    value = int(cmd_list[3])
+                except ValueError:
+                    continue
+                except IndexError:
+                    continue
+
+                if value < 0:
+                    value = 0
+                if value > 9:
+                    value = 9
+                display.set_pixel(x, y, value)
+
+            # clear display command
+            elif cmd_id == 'c':
+                display.clear()
+
+            # analog write command
             # if values are out of range, command is ignored
-            try:
-                pin = int(cmd_list[1])
-                value = int(cmd_list[2])
-            except IndexError:
-                continue
-            except ValueError:
-                continue
+            elif cmd_id == 'a':
+                # check pin and value ranges
+                try:
+                    pin = int(cmd_list[1])
+                    value = int(cmd_list[2])
+                except IndexError:
+                    continue
+                except ValueError:
+                    continue
 
-            if 0 <= pin <= 2:
-                if 0 <= value <= 1:
+                if 0 <= pin <= 2:
+                    if not 0 <= value <= 1023:
+                        value = 256
                     if pin == 0:
-                        pin0.write_digital(value)
+                        pin0.write_analog(value)
                     elif pin == 1:
-                        pin1.write_digital(value)
+                        pin1.write_analog(value)
                     elif pin == 2:
-                        pin2.write_digital(value)
+                        pin2.write_analog(value)
+
+            # digital write command
+            elif cmd_id == 't':
+                # check pin and value ranges
+                # if values are out of range, command is ignored
+                try:
+                    pin = int(cmd_list[1])
+                    value = int(cmd_list[2])
+                    digital_outputs[pin] = True
+                    print(str(digital_outputs))
+                except IndexError:
+                    continue
+                except ValueError:
+                    continue
+
+                if 0 <= pin <= 2:
+                    if 0 <= value <= 1:
+                        if pin == 0:
+                            pin0.write_digital(value)
+                        elif pin == 1:
+                            pin1.write_digital(value)
+                        elif pin == 2:
+                            pin2.write_digital(value)
+                    else:
+                        pass
+
+            elif cmd == 'g':
+                # This string will contain the sensor values and will
+                # be "printed" to the serial port.
+                # Fields are comma delimited
+                sensor_string = ""
+
+                # accelerometer
+                sensor_string += str(accelerometer.get_x()) + ','
+                sensor_string += str(accelerometer.get_y()) + ','
+                sensor_string += str(accelerometer.get_z()) + ','
+
+                # buttons
+                sensor_string += str(button_a.is_pressed()) + ','
+
+                sensor_string += str(button_b.is_pressed()) + ','
+
+                # get digital input pin values
+                if not digital_outputs[0]:
+                    sensor_string += str(pin0.read_digital()) + ','
                 else:
-                    pass
+                    sensor_string += '0' + ','
+                #
+                if not digital_outputs[1]:
+                    sensor_string += str(pin1.read_digital()) + ','
+                else:
+                    sensor_string += '0' + ','
+                #
+                if not digital_outputs[2]:
+                    sensor_string += str(pin2.read_digital()) + ','
+                else:
+                    sensor_string += '0' + ','
 
-        elif cmd == 'g':
-            # This string will contain the sensor values and will
-            # be "printed" to the serial port.
-            # Fields are comma delimited
-            sensor_string = ""
+                # get analog input pin values
+                sensor_string += str(pin0.read_analog()) + ','
 
-            # accelerometer
-            sensor_string += str(accelerometer.get_x()) + ','
-            sensor_string += str(accelerometer.get_y()) + ','
-            sensor_string += str(accelerometer.get_z()) + ','
+                sensor_string += str(pin1.read_analog()) + ','
 
-            # buttons
-            sensor_string += str(button_a.is_pressed()) + ','
+                sensor_string += str(pin2.read_analog())
 
-            sensor_string += str(button_b.is_pressed()) + ','
+                print(sensor_string)
 
-            # get digital input pin values
-            sensor_string += str(pin0.read_digital()) + ','
+            elif cmd == 'v':
+                print('s2mb.py Version 1.07 24 March 2018')
+            else:
+                continue
 
-            sensor_string += str(pin1.read_digital()) + ','
 
-            sensor_string += str(pin2.read_digital()) + ','
-
-            # get analog input pin values
-            sensor_string += str(pin0.read_analog()) + ','
-
-            sensor_string += str(pin1.read_analog()) + ','
-
-            sensor_string += str(pin2.read_analog())
-
-            print(sensor_string)
-
-        elif cmd == 'v':
-            print('s2mb.py Version 1.06 07 December 2017')
-        else:
-            continue
-
+loop([False, False, False])
